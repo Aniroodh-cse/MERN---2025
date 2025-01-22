9122b41853ea0b2afd4cbb6a0ccd205bb035f210
Ram <ram@gmail.com>
2025-01-22 13:54:05 +0530

    Adding 22.01.2025(FN)

--- seceFrontend/src/App.jsx
+++ seceFrontend/src/App.jsx
@@ -6,6 +6,8 @@ import Home from './components/components/FunctionalComponents/Home'
 import Navbar from './components/components/FunctionalComponents/Navbar'
 import {BrowserRouter ,Routes ,Route} from 'react-router-dom'
 import Signup from './components/components/FunctionalComponents/signup'
+import UserState from './components/components/FunctionalComponents/Hooks/useState'
+import UseEffect from './components/components/FunctionalComponents/Hooks/UseEffect'
  function App() {
   return (
     <>
@@ -15,18 +17,18 @@ import Signup from './components/components/FunctionalComponents/signup'
       <Routes>
         <Route path="/" element={<Home />}></Route>
         <Route path="/About" element={<About />}></Route>
+        <Route path="/Contact" element={<Constact />}></Route>
         <Route path="/ClassCompEG" element={<ClassCompEG />}></Route>
         <Route path="/Gallery" element={<Gallery/>}></Route>
+        <Route path="/use-state" element={<Constact />}></Route>
+        <Route path='/useEffect' element={<UseEffect />}></Route>
+        <Route path="/UseState" element={<UserState/>}></Route>
         <Route path="/Signup" element={<Signup/>}></Route>
+        
+        
       </Routes>
       </BrowserRouter>
-    <section>
-    <Home/>
-    <About/>
-    <ClassCompEG/>
-    <Gallery image="sece Logo" page="Gallery"/>
-    <Constact/>
-    </section>
+
     </>
 )
 }
